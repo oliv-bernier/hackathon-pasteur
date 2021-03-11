e247ce75dd94845a3080c55c5d0866e2d6a80016
oliv-bernier <olivbernier@gmail.com>
2021-03-11 22:26:50 +0100

    analysis ok

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -10,20 +10,51 @@ const Details = ({
   tempo,
   freq,
   image,
-  height,
   close,
 }) => {
   const progressbars = [];
 
   for (let i = 0; i < 14; i++) {
+    const bar = Math.floor((Math.random() * 100) + 1);
     progressbars.push(
       <div className="details__zoom-analysis-div-bar">
-        <div className="details__zoom-analysis-div-progress" style={{ height: `${height}%` }} />
+        <div className="details__zoom-analysis-div-progress" style={{ height: `${bar}%` }} />
       </div>,
     );
   }
 
-  return (
+  if (mood === 'Silence') {
+    return (
+      <div className="details">
+        <button type="button" className="details__close" onClick={() => close(false)}>X</button>
+        <div className="details__zoom">
+          <div className="details__zoom-mood">
+            <p className="details__zoom-mood-mood">Mood:</p>
+            <p className="details__zoom-mood-result">{mood}</p>
+          </div>
+          <div className="details__zoom-track">
+            <p className="details__zoom-track-title">Morceau Similaire :</p>
+            <p className="details__zoom-track-band">{band}</p>
+            <p className="details__zoom-track-name">{track}</p>
+            <img className="details__zoom-track-image" src={image} alt="test" />
+          </div>
+          <div className="details__zoom-tempo">
+            <p className="details__zoom-tempo-title">Tempo : {tempo}</p>
+            <p className="details__zoom-tempo-freq">Freq : {freq}</p>
+          </div>
+          <div className="details__zoom-analysis">
+            <p className="details__zoom-analysis-title">Analyse de fréquence :</p>
+            <div className="details__zoom-analysis-div" />
+            <div className="details__zoom-analysis-details">
+              <p>Grave</p>
+              <p>Medium</p>
+              <p>Aigue</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } return (
     <div className="details">
       <button type="button" className="details__close" onClick={() => close(false)}>X</button>
       <div className="details__zoom">
@@ -64,7 +95,6 @@ Details.propTypes = {
   tempo: PropTypes.any.isRequired,
   freq: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  height: PropTypes.number.isRequired,
   close: PropTypes.func.isRequired,
 };
 

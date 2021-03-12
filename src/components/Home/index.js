2f2520668d48ed45037aa6d83acfd267259a60c7
oliv-bernier <olivbernier@gmail.com>
2021-03-12 10:12:24 +0100

    css positioning fixes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -24,23 +24,23 @@ const Home = () => {
     <div className="home">
       <div className="home__map">
         <img className="home__map-image" src={map} alt="Carte en coupe du premier étage de Pasteur" />
-      </div>
-      {icons.map((icon, index) => (
-        <div>
-          <div
-            className="home__icon"
-            onClick={() => {
-              findDetails(index);
-              setDetails(true);
-            }}
-          >
-            <img key={icon.mood} className={`home__icon-image home__icon-image--${icon.class}`} src={icon.image} alt="Icone d'ambiance" />
+        {icons.map((icon, index) => (
+          <div>
+            <div
+              className="home__icon"
+              onClick={() => {
+                findDetails(index);
+                setDetails(true);
+              }}
+            >
+              <img key={icon.mood} className={`home__icon-image home__icon-image--${icon.class}`} src={icon.image} alt="Icone d'ambiance" />
+            </div>
+            {isDetails && (
+              <Details {...data[detailsIndex]} close={setDetails} />
+            )}
           </div>
-          {isDetails && (
-            <Details {...data[detailsIndex]} close={setDetails} />
-          )}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
